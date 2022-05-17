07d996af76b5e52777540632d425154b4e52fc84
João Victor Caires <78885438+srkayrez@users.noreply.github.com>
2022-05-17 08:11:07 -0300

    Add files via upload

---
+++ Ex2/Ex2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Ex2
+{
+    static void Main(String[] args)
+    {
+        //Desenvolver um algoritmo que leia a cotação do euro converta para o Real,
+        //calcule a compra de 750 euros e imprima o resultado;
+
+        double euroHoje, euros, valorFinal ;
+        Console.WriteLine("Digite o valor do euro hoje: ");
+        euroHoje = double.Parse(Console.ReadLine());
+        Console.WriteLine($"O euro esta custando: R${euroHoje} hoje.");
+        Console.WriteLine("Digite a quantia em euros: ");
+        euros = double.Parse(Console.ReadLine());
+        valorFinal = euroHoje * euros;
+       
+        Console.WriteLine($"O valor de {euros} euros é {valorFinal} reais");
+
+
+    }
+}
