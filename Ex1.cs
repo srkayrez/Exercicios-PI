e90adbf4806590af47b99df7d8b8eaa16da66c74
João Victor Caires <78885438+srkayrez@users.noreply.github.com>
2022-05-17 07:09:21 -0300

    Update Ex1.cs

--- Ex1.cs
+++ Ex1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 class Programa
 {
     static void Main(string[] args)
@@ -7,8 +7,17 @@ class Programa
         //sabendo que o aluno mais novo tem 20 anos e o mais velho tem 50 anos,
         //utilize a seguinte fórmula para calcular: MEDIA ALUNO = (IDADE MÍNIMA + IDADE MÁXIMA) / 2;
 
-        int idadeMedia, idadeMinima = 20, idadeMaxima = 50;
+        //int idadeMedia, idadeMinima = 20, idadeMaxima = 50;
+        int idadeMedia, idadeMinima, idadeMaxima;
+
+        Console.WriteLine("Digite a idade minima da turma: ");
+        idadeMinima = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a idade maxima da turma: ");
+        idadeMaxima = int.Parse(Console.ReadLine());
+
         idadeMedia = (idadeMinima + idadeMaxima) / 2;
+
         Console.WriteLine($"A menor idade é de: {idadeMinima}");
         Console.WriteLine($"A maior idade é de: {idadeMaxima}");
         Console.WriteLine($"A idade media é de: {idadeMedia}");
